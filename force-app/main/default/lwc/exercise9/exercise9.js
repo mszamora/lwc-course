d997e204aedef45c8c5cec4ba3be14bfee70a880
Oriol Farràs <ofarras@cloudcoachers.com>
2023-02-07 12:24:53 +0100

    fix ej. 9

--- force-app/main/default/lwc/exercise9/exercise9.js
+++ force-app/main/default/lwc/exercise9/exercise9.js
@@ -1,4 +1,4 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement,api } from "lwc";
 
 export default class Exercise9 extends LightningElement {
     @api itemName = "Default name";
