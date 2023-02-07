9c8778ba3402d87861d525c1b63062a2bd044c1b
Oriol Farràs <ofarras@cloudcoachers.com>
2023-02-07 12:24:32 +0100

    ejercicio 9

---
+++ force-app/main/default/lwc/exercise9/exercise9.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from "lwc";
+
+export default class Exercise9 extends LightningElement {
+    @api itemName = "Default name";
+}
---
+++ force-app/main/default/lwc/exercise9Wrapper/exercise9Wrapper.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Exercise9Wrapper extends LightningElement {}
