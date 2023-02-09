034d60212fa1b74eb3a7d966c93fe8b54a89b3da
Oriol Farràs <ofarras@cloudcoachers.com>
2023-02-09 10:40:48 +0100

    Fix 21

--- force-app/main/default/lwc/exercise21/exercise21.js
+++ force-app/main/default/lwc/exercise21/exercise21.js
@@ -4,9 +4,13 @@ export default class Exercise21 extends LightningElement {
     handleClick() {
         const propertiesSelectedEvent = new CustomEvent("propertiesselected", {
           detail: {
-            bold: true,
-            italics: false,
-            color: "#fff"
+            añadircuenta: true,
+            crearoportunidad: false,
+            datosextra: [   
+                {producto: "Llave",Cantidad: 1},
+                {producto: "Destornillador",Cantidad: 3},
+                {producto: "Tuerca",Cantidad: 100}],
+            idContacto:"1341243fff"
           }
         });
     
